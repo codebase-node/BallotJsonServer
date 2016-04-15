c8dccd9b35975497ef6897adb511f41df877ef15
aromal <aromalsasidharan@gmail.com>
2016-04-15 16:51:43 +0530

    ballot mock api added

---
+++ server.js
@@ -0,0 +1,27 @@
+var jsonServer = require('json-server');
+
+var server = jsonServer.create(); // Returns an Express server
+var router = jsonServer.router('ballot-api.json'); // Returns an Express router
+
+router.render = function (req, res) {
+    res.jsonp({
+
+    meta : {status:"ok", message:"successfull",code:200},
+
+        body: res.locals.data
+    })
+
+   
+
+}
+
+
+
+
+
+
+server.use(jsonServer.defaults()); // logger, static and cors middlewares
+server.use(router); // Mount router on '/'
+// consol.log("here")
+server.listen(8000);
+
